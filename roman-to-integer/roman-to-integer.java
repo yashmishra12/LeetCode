ccbeab225ff14e094a78a8fbf35a539451fd726d
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2021-12-27 13:38:15 -0500

    Update roman-to-integer.java
    used charAt function instead of extra space.

--- roman-to-integer/roman-to-integer.java
+++ roman-to-integer/roman-to-integer.java
@@ -10,19 +10,19 @@ class Solution {
         map.put('D', 500);
         map.put('M', 1000);
         
-        char[] arr = s.toCharArray();
         
-        int res = map.get(arr[0]);
+        int res = map.get(s.charAt(0));
         int val = 0;
         
-        for(int i=1; i<arr.length; i++) {
+        for(int i=1; i<s.length(); i++) {
             
-            char cur =  arr[i];
+            char cur = s.charAt(i);
+            
             val = map.get(cur);
             
             res += val;
             
-            int preVal = map.get(arr[i-1]);
+            int preVal = map.get(s.charAt(i-1));
             if(val>preVal) {
                 res = res - preVal - preVal;
             }
