d8efb63111ed218a96062e90985936b494460c20
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2021-12-29 02:59:52 -0500

    Update count-of-smaller-numbers-after-self.java
    merge sort used

--- count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.java
+++ count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.java
@@ -70,64 +70,68 @@
 // }
 
 class Solution {
-    public List<Integer> countSmaller(int[] nums) {
-        int n = nums.length;
-        int[] result = new int[n];
-        int[] indices = new int[n]; // record the index. we are going to sort this array
-        for (int i = 0; i < n; i++) {
-            indices[i] = i;
+    int[] count;
+     public List<Integer> countSmaller(int[] nums) {
+         count = new int[nums.length];
+         List<Integer> res = new ArrayList<>();
+         int[] index = new int[nums.length];
+         for(int i=0; i<index.length; i++) {
+             index[i] = i;
+         }
+         
+         split(nums, index, 0, nums.length-1);
+         
+         for(int i=0; i<nums.length; i++) res.add(count[i]);
+         
+         return res;
+     }
+    
+    public void split(int[] arr, int[] index, int l, int r) {
+        if(l<r) {
+            int mid = l + (r-l)/2;
+            split(arr, index, l, mid);
+            split(arr, index, mid+1, r);
+            merge(arr, index, l, mid, r);
         }
-        // sort indices with their corresponding values in nums, i.e., nums[indices[i]]
-        mergeSort(indices, 0, n, result, nums);
-        // change int[] to List to return
-        List<Integer> resultToReturn = new ArrayList<Integer>(n);
-        for (int i : result) {
-            resultToReturn.add(i);
-        }
-        return resultToReturn;
     }
-
-    private void mergeSort(int[] indices, int left, int right, int[] result, int[] nums) {
-        if (right - left <= 1) {
-            return;
-        }
-        int mid = (left + right) / 2;
-        mergeSort(indices, left, mid, result, nums);
-        mergeSort(indices, mid, right, result, nums);
-        merge(indices, left, right, mid, result, nums);
-    }
-
-    private void merge(int[] indices, int left, int right, int mid, int[] result, int[] nums) {
-        // merge [left, mid) and [mid, right)
-        int i = left; // current index for the left array
-        int j = mid; // current index for the right array
-        // use temp to temporarily store sorted array
-        List<Integer> temp = new ArrayList<Integer>(right - left);
-        while (i < mid && j < right) {
-            if (nums[indices[i]] <= nums[indices[j]]) {
-                // j - mid numbers jump to the left side of indices[i]
-                result[indices[i]] += j - mid;
-                temp.add(indices[i]);
+    
+    public void merge(int[] arr, int[] index, int l, int mid, int r) {
+        int n1 = mid-l+1;
+        int n2 = r-mid;
+        
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+        
+        for(int i =0; i<n1; i++) L[i] = index[l+i];
+        for(int i =0; i<n2; i++) R[i] = index[mid+1+i];
+        
+        int i=0, j = 0, k=l, rightCount=0;
+        
+        while(i<n1 && j<n2) {
+            if(arr[L[i]]<= arr[R[j]]) {
+                index[k] = L[i];
+                count[L[i]] += rightCount;
                 i++;
             } else {
-                temp.add(indices[j]);
+                index[k] = R[j];
+                rightCount++;
                 j++;
             }
+            k++;
         }
-        // when one of the subarrays is empty
-        while (i < mid) {
-            // j - mid numbers jump to the left side of indices[i]
-            result[indices[i]] += j - mid;
-            temp.add(indices[i]);
-            i++;
+        
+        while(i<n1) {
+              index[k] = L[i];
+                count[L[i]] += rightCount;
+                i++;
+            k++;
         }
-        while (j < right) {
-            temp.add(indices[j]);
-            j++;
-        }
-        // restore from temp
-        for (int k = left; k < right; k++) {
-            indices[k] = temp.get(k - left);
+        
+        while(j<n2) {
+             index[k] = R[j];
+                rightCount++;
+                j++;
+            k++;
         }
     }
 }
