071f20c016b3d6ba4963fd9443955df0bccf668f
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2021-12-31 19:26:51 -0500

    Update palindrome-number.java

--- palindrome-number/palindrome-number.java
+++ palindrome-number/palindrome-number.java
@@ -1,19 +1,38 @@
+// class Solution {
+//     public boolean isPalindrome(int x) {
+//         if(x<0) return false;
+        
+//         Integer temp = new Integer(x);
+        
+//         String num = temp.toString();
+        
+//         int i = 0;
+//         int j = num.length()-1;
+        
+//         while(i<j) {
+//             if(num.charAt(i)!=num.charAt(j)) return false;
+//             i++;
+//             j--;
+//         }
+//         return true;
+//     }
+// }
+
 class Solution {
     public boolean isPalindrome(int x) {
         if(x<0) return false;
         
-        Integer temp = new Integer(x);
+        int rev = 0;
+        int rem = 0;
+        int orig = x;
         
-        String num = temp.toString();
-        
-        int i = 0;
-        int j = num.length()-1;
-        
-        while(i<j) {
-            if(num.charAt(i)!=num.charAt(j)) return false;
-            i++;
-            j--;
+        while(x!=0) {
+            rem = x%10;
+            
+            rev = rev*10 + rem;
+            x = x/10;
         }
-        return true;
+     
+        return orig==rev;
     }
 }
