a42ff50036ec7da93a02e6acf964bec8aad7e2f4
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-01-11 19:43:57 -0500

    Time: 318 ms (12.90%), Space: 114 MB (6.44%) - LeetHub

---
+++ 79-word-search/79-word-search.java
@@ -0,0 +1,38 @@
+class Solution {
+    char[][] board;
+    int rowSize;
+    int colSize;
+    
+    public boolean exist(char[][] board, String word) {
+        this.board = board;
+        this.rowSize = board.length;
+        this.colSize = board[0].length;
+        
+        for(int i=0; i<this.rowSize; ++i)
+            for(int j=0; j<this.colSize; ++j)
+                if(helper(i, j, word, 0)) return true;
+               
+        return false;
+    }
+    
+    public boolean helper(int row, int col, String word, int index) {
+        if(index>=word.length()) return true;
+        
+        if(row<0 || row==this.rowSize || col<0 || col==this.colSize || this.board[row][col]!=word.charAt(index)) return false;
+        
+        boolean ret = false;
+        
+        this.board[row][col] = '#';
+        
+        int[] x = {0, 1, 0, -1};
+        int[] y = {1, 0, -1, 0};
+        
+        for(int d=0; d<4; d++) {
+            ret = helper(row+x[d], col+y[d], word, index+1);
+            if(ret) break;
+        }
+        
+        this.board[row][col] = word.charAt(index);
+        return ret;
+        }
+}
