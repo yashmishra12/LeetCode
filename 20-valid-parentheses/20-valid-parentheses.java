6741fca5bbc2e891c7d818592a9fea1ee31dd677
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-01-16 21:24:20 -0500

    Time: 1 ms (99.08%), Space: 37.1 MB (80.23%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,28 +1,41 @@
 class Solution {
     public boolean isValid(String s) {
-    
+        
         if(s.length()==1) return false;
         
-     Stack<Character> st = new Stack<>();
+        Stack<Character> st=new Stack<>();
         
-        for(int i=0; i<s.length(); i++) {
-            Character cur = s.charAt(i);
-            
-            if(cur=='(' || cur=='[' || cur=='{') {
-                st.push(cur);
-            } else {
+        for(int i=0; i<s.length(); i++){
+
+            if(s.charAt(i)==')'){
                 if(st.isEmpty()) return false;
-                Character top = st.pop();
                 
-                if(cur==')' && top!='(')    return false; 
-                
-                else if (cur==']' && top != '[') return false;
-                
-                else if (cur=='}' && top != '{') return false;
-                        
+                if(st.peek()=='(')
+                    st.pop();
+                else return false;
             }
-        }
+             else if(s.charAt(i)==']'){
+                 if(st.isEmpty()) return false;
+                
+                 if(st.peek()=='[')
+                    st.pop();
+                 else return false;
+            }
+             else if(s.charAt(i)=='}'){
+                 if(st.isEmpty()) return false;
+                
+                 if(st.peek()=='{')
+                    st.pop();
+                 else return false;
+            }
+            else{
+               st.push(s.charAt(i)); 
+            }
+
+    } 
         
-        return st.isEmpty() ? true:false;
+        boolean res =  (st.isEmpty()) ?  true :  false;
+        return res;
     }
 }
+
