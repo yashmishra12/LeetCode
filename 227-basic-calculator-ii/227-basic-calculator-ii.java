0123e824b10262803f26a3eff587c7cdfd3f51dd
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-01-26 18:45:08 -0500

    Time: 8 ms (79.10%), Space: 39 MB (77.65%) - LeetHub

--- 227-basic-calculator-ii/227-basic-calculator-ii.java
+++ 227-basic-calculator-ii/227-basic-calculator-ii.java
@@ -1,7 +1,6 @@
 class Solution {
     public int calculate(String s) {
        if(s==null || s.isEmpty()) return 0;
-        s = s.trim();
         int len = s.length();
         
         Stack<Integer> st = new Stack<>();
@@ -10,7 +9,6 @@ class Solution {
         
         for(int i=0; i<len; i++) {
             char cur = s.charAt(i);
-            if(Character.isSpace(cur)) continue;
             if(Character.isDigit(cur)) curNum = curNum*10 + (cur - '0');
             
             if(!Character.isDigit(cur) && !Character.isWhitespace(cur) || i==len-1) {
