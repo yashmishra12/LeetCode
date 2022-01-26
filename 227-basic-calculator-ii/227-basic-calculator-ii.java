35a676a6a0ac0c03e5b991250074aa534e65537a
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-01-26 18:41:57 -0500

    Time: 9 ms (70.62%), Space: 39.3 MB (56.05%) - LeetHub

---
+++ 227-basic-calculator-ii/227-basic-calculator-ii.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int calculate(String s) {
+       if(s==null || s.isEmpty()) return 0;
+        int len = s.length();
+        
+        Stack<Integer> st = new Stack<>();
+        int curNum = 0;
+        char operation = '+';
+        
+        for(int i=0; i<len; i++) {
+            char cur = s.charAt(i);
+            if(Character.isDigit(cur)) curNum = curNum*10 + (cur - '0');
+            
+            if(!Character.isDigit(cur) && !Character.isWhitespace(cur) || i==len-1) {
+                if(operation == '-') st.push(-curNum);
+                else if(operation=='+') st.push(curNum);
+                else if(operation=='*') st.push(st.pop()*curNum);
+                else if(operation=='/') st.push(st.pop()/curNum);
+                
+                operation = cur;
+                curNum = 0;
+            }
+        }
+        
+        int res = 0;
+        
+        while(!st.isEmpty()) res+=st.pop();
+        
+        return res;
+    }
+}
