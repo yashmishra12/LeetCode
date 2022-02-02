ee014f22915b1808e178162a6008f1057105e980
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-02-02 10:50:20 -0500

    Time: 212 ms (30.53%), Space: 68.6 MB (6.85%) - LeetHub

--- 204-count-primes/204-count-primes.java
+++ 204-count-primes/204-count-primes.java
@@ -7,7 +7,7 @@ class Solution {
         for(int p=2; p<=(int)Math.sqrt(n); p++) {
             if(numbers[p]==false) {
                 for(int j=p*p; j<n; j=j+p) 
-                    numbers[j] = true; //primes smaller than p^2 would be covered by numbers smaller than p
+                    {numbers[j] = true;} //primes smaller than p^2 would be covered by numbers smaller than p
             }    
         }
         
