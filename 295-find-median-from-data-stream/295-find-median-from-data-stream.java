8bbe6e1811bb4200dc5e0ab0f2110c649c7b4ce8
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-02-03 21:56:08 -0500

    Time: 86 ms (93.33%), Space: 65.1 MB (95.77%) - LeetHub

--- 295-find-median-from-data-stream/295-find-median-from-data-stream.java
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.java
@@ -1,26 +1,45 @@
  class MedianFinder {
- 	PriorityQueue<Integer> pq1;
- 	PriorityQueue<Integer> pq2;
+ 	static PriorityQueue<Integer> maxHeap;
+ 	static PriorityQueue<Integer> minHeap;
  	/** initialize your data structure here. */
  	public MedianFinder() {
- 		pq2 = new PriorityQueue<>();
- 		pq1 = new PriorityQueue<>((a,b)->b-a);
+ 		minHeap = new PriorityQueue<>();
+ 		maxHeap = new PriorityQueue<>((a,b)->b-a);
  	}
  	
  	public void addNum(int num) {
- 		pq1.offer(num);
- 		pq2.offer(pq1.poll());
- 		if(pq1.size() < pq2.size())
- 			pq1.offer(pq2.poll());
+ 		// pq1.offer(num);
+ 		// pq2.offer(pq1.poll());
+ 		// if(pq1.size() < pq2.size())
+ 		// 	pq1.offer(pq2.poll());
+        if(maxHeap.size()==0 || maxHeap.peek()>num) maxHeap.add(num);
+        else minHeap.add(num);
+        
+        rebalance();
  	}
  	
  	public double findMedian() {
- 		if(pq1.size() == pq2.size()){
- 			return (double)(pq1.peek() + pq2.peek()) / 2;
- 		}else{
- 			return (double) pq1.peek();
- 		}
+ 		// if(pq1.size() == pq2.size()){
+ 		// 	return (double)(pq1.peek() + pq2.peek()) / 2;
+ 		// }else{
+ 		// 	return (double) pq1.peek();
+ 		// }
+         if(minHeap.size()==maxHeap.size()) 
+           return ((double)minHeap.peek() + (double)maxHeap.peek())/2;
+        else if(minHeap.size()>maxHeap.size())
+            return (double)minHeap.peek();
+        else
+            return (double)maxHeap.peek();
  	}
+     
+         public static void rebalance() {
+        
+        if(maxHeap.size()-minHeap.size()==2) 
+            {minHeap.add(maxHeap.remove());}
+        else if (minHeap.size()-maxHeap.size()==2) 
+            {maxHeap.add(minHeap.remove());}
+        
+    }
  }
 
  /**
