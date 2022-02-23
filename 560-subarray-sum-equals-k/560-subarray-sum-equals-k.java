fb700281e14ac5b3761addf53d4fe373c7fcde01
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-02-22 20:48:27 -0500

    Time: 43 ms (42.58%), Space: 61.4 MB (32.62%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
@@ -8,7 +8,7 @@ class Solution {
         for(int i=0; i<arr.length; i++) {
             curSum = curSum+arr[i];
             if(map.containsKey(curSum - target))
-                    count = count + map.get(curSum - target);
+                    {count = count + map.get(curSum - target);}
             map.put(curSum, map.getOrDefault(curSum, 0)+1);
         }
         return count;
