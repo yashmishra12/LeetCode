21a90f7d92603d5aecebcaca42af6bb6a39e8d3e
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-03-01 15:17:51 -0500

    Time: 0 ms (100.00%), Space: 42.9 MB (22.55%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -5,9 +5,9 @@ class Solution {
         for (int p=m+n-1; p>=0; p--) {
             if(p2<0) break;
             
-            if(p1>=0 && nums1[p1]>nums2[p2]) nums1[p]=nums1[p1--];
+            if(p1>=0 && nums1[p1]>nums2[p2]) {nums1[p]=nums1[p1--];}
             
-            else nums1[p]=nums2[p2--];
+            else {nums1[p]=nums2[p2--];}
         }
     }
 }
