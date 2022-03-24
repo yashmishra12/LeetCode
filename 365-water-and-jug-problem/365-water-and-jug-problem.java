e79ad6b20060154793aa44801249c6eff4f8db68
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-03-24 14:23:46 -0400

    Time: 124 ms (29.92%), Space: 109 MB (24.15%) - LeetHub

--- 365-water-and-jug-problem/365-water-and-jug-problem.java
+++ 365-water-and-jug-problem/365-water-and-jug-problem.java
@@ -1,12 +1,29 @@
 class Solution {
     public boolean canMeasureWater(int x, int y, int z) {
-        if(x+y<z) return false;
         
-        return z%gcd(x,y)==0;
-    }
-    
-    public static int gcd(int x, int y){
-        if (y==0) return x;
-        return gcd(y, x%y);
+        if(x+y<z) return false;
+        int[] dirs = {x, y,-x, -y};
+        Deque<Integer> q = new ArrayDeque<>();
+        HashSet<Integer> seen = new HashSet<>();        
+        q.offer(0);
+
+        while(q.size()>0){
+            int cur = q.poll();
+            if(cur==z) return true;
+
+            for(int d: dirs){
+                int newNode = cur+d;
+                if (newNode<0 || newNode>x+y) continue;
+                if (newNode==z) return true;
+                
+                if(!seen.contains(newNode)) {
+                    q.offer(newNode);  
+                    seen.add(newNode);
+                }
+            }
+        }
+        
+          return false;
     }
 }
+
