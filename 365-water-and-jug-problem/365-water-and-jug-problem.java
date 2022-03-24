fe0ab364cc949141331f7e8f5d570133051e0961
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-03-24 14:24:05 -0400

    Time: 153 ms (24.15%), Space: 108.4 MB (24.81%) - LeetHub

--- 365-water-and-jug-problem/365-water-and-jug-problem.java
+++ 365-water-and-jug-problem/365-water-and-jug-problem.java
@@ -25,5 +25,15 @@ class Solution {
         
           return false;
     }
+    
+    //         if(x+y<z) return false;
+        
+//         return z%gcd(x,y)==0;
+//     }
+    
+//     public static int gcd(int x, int y){
+//         if (y==0) return x;
+//         return gcd(y, x%y);
+//     }
 }
 
