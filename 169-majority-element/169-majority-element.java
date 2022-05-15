0535d293bdc7b6d5a150e8402c12ad4ba3e976c1
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-05-15 01:33:18 -0400

    Time: 18 ms (16.56%), Space: 55.3 MB (63.11%) - LeetHub

---
+++ 169-majority-element/169-majority-element.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        int curMax = Integer.MIN_VALUE;
+        int res = -1;
+        
+        for(int a: nums) {
+            int freq = map.getOrDefault(a, 0+1)+1;
+            map.put(a, freq);
+            
+            if(curMax<freq){
+                curMax = Math.max(curMax, freq);
+                res = a;
+            }
+        }
+        
+        return res;
+        
+    }
+}
