5d20f8581f441b0faa3266fe571adf4c1bf26b7d
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-05-16 16:43:05 -0400

    Update 15-3sum.java

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,16 +1,54 @@
+// class Solution {
+//     public List<List<Integer>> threeSum(int[] nums) {  
+//         if (nums.length<3) return new ArrayList<List<Integer>>();
+//         Set<String> set = new HashSet<String>();
+
+//         Arrays.sort(nums);
+        
+//         ArrayList<List<Integer>> res = new ArrayList<List<Integer>>();
+        
+//         int i = 0, j = 0;
+        
+//         for(int p=0; p<nums.length-2; p++) {
+//             i = p+1;
+//             j = nums.length-1;
+            
+//             while(i<j){
+//                  int curSum = nums[p]+nums[i]+nums[j];
+//                 String pot = stringify(nums[p],nums[i],nums[j]);
+            
+//                 if(curSum==0 && !set.contains(pot) ){
+//                     ArrayList<Integer> ans = new ArrayList<>();
+//                     ans.add(nums[p]);
+//                     ans.add(nums[i++]);
+//                     ans.add(nums[j--]);
+//                     res.add(ans);
+//                     set.add(pot);
+                    
+//                 }
+//                 else if(curSum<0) {
+//                     i++;
+//                 } else {
+//                     j--;
+//                 }
+//             }
+//         }
+        
+//         return res;
+//     }
+    
+//     public static String stringify(int a, int b, int c) {
+//         return a+","+b+","+c;
+//     }
+// }
+    
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        
+         
         if (nums.length<3) return new ArrayList<List<Integer>>();
-        // Set<Integer> set = Arrays.stream(pnums).boxed().collect(Collectors.toSet());
-        // Integer[] nums = new Integer[set.size()];
-        // set.toArray(nums);
+        Set<List<Integer>> set = new HashSet<>();
         
-        Set<String> set = new HashSet<String>();
-
         Arrays.sort(nums);
-        
-        ArrayList<List<Integer>> res = new ArrayList<List<Integer>>();
         
         int i = 0, j = 0;
         
@@ -20,29 +58,15 @@ class Solution {
             
             while(i<j){
                  int curSum = nums[p]+nums[i]+nums[j];
-                String pot = stringify(nums[p],nums[i],nums[j]);
             
-                if(curSum==0 && !set.contains(pot) ){
-                    ArrayList<Integer> ans = new ArrayList<>();
-                    ans.add(nums[p]);
-                    ans.add(nums[i++]);
-                    ans.add(nums[j--]);
-                    res.add(ans);
-                    set.add(pot);
-                    
+                if(curSum==0 ){
+                    set.add(Arrays.asList(nums[p], nums[i++], nums[j--]));
                 }
-                else if(curSum<0) {
-                    i++;
-                } else {
-                    j--;
-                }
+                else if(curSum<0)  {i++;}
+                else  {j--;}
             }
         }
         
-        return res;
-    }
-    
-    public static String stringify(int a, int b, int c) {
-        return a+","+b+","+c;
+        return new ArrayList<>(set);
     }
 }
