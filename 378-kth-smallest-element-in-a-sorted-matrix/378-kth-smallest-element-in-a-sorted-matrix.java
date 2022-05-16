1a719af40b12a154777db453eec551379a7f43fd
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-05-16 17:35:11 -0400

    Time: 124 ms (5.00%), Space: 57 MB (39.11%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
@@ -15,8 +15,8 @@ class Solution {
     
     public static void reArrange (int[][] matrix, int row, int col){
         int newRow = 0, newCol = 0;
-        
-        if(row==0 && col==0 || matrix[row][col]==Integer.MIN_VALUE) {
+        if(matrix[row][col]==Integer.MIN_VALUE) return;
+        if(row==0 && col==0) {
             matrix[row][col] = Integer.MIN_VALUE;
             return;
         }
