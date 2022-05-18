6a586bc8a60d11bb109c5a860273a0f024663ea1
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-05-18 15:33:34 -0400

    Time: 0 ms (100.00%), Space: 42.7 MB (7.37%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.java
+++ 54-spiral-matrix/54-spiral-matrix.java
@@ -1,37 +1,37 @@
 class Solution {
-    public List<Integer> spiralOrder(int[][] array) {
-       List<Integer> res = new ArrayList<>();
-        int sr = 0;
-        int sc = 0;
-        int er = array.length-1;
-        int ec = array[0].length-1;
-
-        while(sr<=er && sc<=ec){
-					
-            for(int i = sc; i<=ec; i++){
-                res.add(array[sr][i]);
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int sr=0, sc = 0, er = matrix.length-1, ec = matrix[0].length-1;
+        
+        List<Integer> res = new ArrayList<>();
+        
+        while(sr<=er && sc<=ec) {
+            
+            for(int i=sc; i<=ec; i++) {
+                res.add(matrix[sr][i]);
             }
-					
-            for(int i = sr+1; i<=er; i++){
-                res.add(array[i][ec]);
+            
+            for(int i=sr+1; i<=er; i++) {
+                res.add(matrix[i][ec]);
             }
-					
-            for(int i=ec-1; i>=sc; i--){
+            
+            
+            for(int i=ec-1; i>=sc; i--) {
                 if(sr==er) break;
-                res.add(array[er][i]);
+                res.add(matrix[er][i]);
             }
-					
+            
+            
             for(int i=er-1; i>sr; i--){
                 if(ec==sc) break;
-                res.add(array[i][sc]);
+                res.add(matrix[i][sc]);
             }
-					
-            sc++; // -->
-            ec--; // <--
-            sr++; // V
-            er--; // ^
+            
+            sr++;
+            er--;
+            sc++;
+            ec--;
         }
-
+        
         return res;
     }
 }
