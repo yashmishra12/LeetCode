5702d07cfb14bb31fdb78dd80913903346791a0b
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-05-19 15:24:03 -0400

    Time: 29 ms (26.73%), Space: 54.3 MB (33.48%) - LeetHub

--- 277-find-the-celebrity/277-find-the-celebrity.java
+++ 277-find-the-celebrity/277-find-the-celebrity.java
@@ -5,21 +5,21 @@ public class Solution extends Relation {
     public int findCelebrity(int n) {
         
         Stack<Integer> st = new Stack<>();
-        for(int i=0; i<n; i++) st.push(i);
+        for(int i=0; i<n; i++) {st.push(i);}
         
         while(st.size()>=2){
             int a = st.pop();
             int b = st.pop();
             
-            if(knows(a, b)) st.push(b);
-                else st.push(a);
+            if(knows(a, b)) {st.push(b);}
+            else {st.push(a);}
         }
         
         int pot = st.pop();
         
         for(int i=0; i<n; i++) {
             if(i!=pot) {
-                if (!knows(i, pot) || knows(pot, i)) return -1;
+                if (!knows(i, pot) || knows(pot, i)){ return -1;}
             }
         }
         
