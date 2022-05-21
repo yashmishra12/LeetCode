d92b0019be510bb3f7e637bd5600c06b004ba5ad
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-05-21 15:05:08 -0400

    Time: 8 ms (16.91%), Space: 51.8 MB (42.34%) - LeetHub

---
+++ 135-candy/135-candy.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int[] arr = new int[ratings.length];
+        
+        Arrays.fill(arr, 1);
+        
+        for(int i=1; i<arr.length; i++) {
+            if(ratings[i]>ratings[i-1]) arr[i] = arr[i-1]+1;
+        }
+        
+         for(int i=arr.length-2; i>=0; i--) {
+            if(ratings[i]>ratings[i+1]) arr[i] = Math.max(arr[i],arr[i+1]+1);
+        }
+        
+        return Arrays.stream(arr).sum();
+        
+        
+        
+    }
+}
