e1f7d01665b4bb17191742f305fefe964be59b8c
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-05-25 16:57:36 -0400

    Time: 2 ms (83.26%), Space: 44 MB (68.90%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.java
+++ 1094-car-pooling/1094-car-pooling.java
@@ -1,13 +1,18 @@
 class Solution {
     public boolean carPooling(int[][] trips, int capacity) {
-        int[] res = new int[1001];
+        int[] res = new int[1001]; //mentioned that there are 1000 locations
         
-        for(int[] t: trips) {
-            for(int i=t[1]; i<t[2]; i++) { //i doen't go till t[2] because people are boarded-off
-                res[i] += t[0];
-                if(res[i]>capacity) return false;
-            }
+        for(int[] t: trips) {            
+            res[t[1]] += t[0];
+            res[t[2]] -= t[0];
         }
+        
+        for(int n: res) {
+            capacity = capacity - n;
+            if(capacity<0) return false;
+        }
+        
+        
         return true;
     }
 }
