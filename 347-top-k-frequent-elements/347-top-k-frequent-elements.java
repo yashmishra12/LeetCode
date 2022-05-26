ba61134a2818830a68944517d296c7f5e95c025c
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-05-26 17:48:42 -0400

    Time: 19 ms (40.83%), Space: 50.6 MB (43.36%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -1,33 +1,69 @@
+// class Solution {
+//     public int[] topKFrequent(int[] nums, int k) {
+        
+//         HashMap<Integer, Integer> hm = new HashMap<>();
+        
+//         for(int a: nums) {
+//             hm.put(a, hm.getOrDefault(a,0)+1);
+//         }
+        
+//         int[] res = new int[k];
+//         int i=0;
+//         int[] kvPair = new int[2];
+        
+//         while(k>0) {
+//             for(Map.Entry<Integer,Integer> a: hm.entrySet()) {
+//                 int key = a.getKey();
+//                 int freq = hm.get(key);
+                
+//                 if(freq>kvPair[1]) {
+//                     kvPair[0] = key;
+//                     kvPair[1] = freq;   
+//                 }
+//             }
+            
+//             res[i++] = kvPair[0];
+//             hm.remove(kvPair[0]);
+//             Arrays.fill(kvPair, 0);
+//             k--;
+//         }
+        
+//        return res; 
+//     }
+// }
+
+
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        
-        HashMap<Integer, Integer> hm = new HashMap<>();
-        
-        for(int a: nums) {
-            hm.put(a, hm.getOrDefault(a,0)+1);
+        // O(1) time
+        if (k == nums.length) {
+            return nums;
         }
         
-        int[] res = new int[k];
-        int i=0;
-        int[] kvPair = new int[2];
-        
-        while(k>0) {
-            for(Map.Entry<Integer,Integer> a: hm.entrySet()) {
-                int key = a.getKey();
-                int freq = hm.get(key);
-                
-                if(freq>kvPair[1]) {
-                    kvPair[0] = key;
-                    kvPair[1] = freq;   
-                }
-            }
-            
-            res[i++] = kvPair[0];
-            hm.remove(kvPair[0]);
-            Arrays.fill(kvPair, 0);
-            k--;
+        // 1. build hash map : character and how often it appears
+        // O(N) time
+        Map<Integer, Integer> count = new HashMap();
+        for (int n: nums) {
+          count.put(n, count.getOrDefault(n, 0) + 1);
         }
-        
-       return res; 
+
+        // init heap 'the less frequent element first'
+        Queue<Integer> heap = new PriorityQueue<>(
+            (n1, n2) -> count.get(n1) - count.get(n2));
+
+        // 2. keep k top frequent elements in the heap
+        // O(N log k) < O(N log N) time
+        for (int n: count.keySet()) {
+          heap.add(n);
+          if (heap.size() > k) heap.poll();    
+        }
+
+        // 3. build an output array
+        // O(k log k) time
+        int[] top = new int[k];
+        for(int i = k - 1; i >= 0; --i) {
+            top[i] = heap.poll();
+        }
+        return top;
     }
 }
