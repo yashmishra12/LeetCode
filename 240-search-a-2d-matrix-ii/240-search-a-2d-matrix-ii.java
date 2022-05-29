d50411f082c326f50fc09cec1ee2c5e40e9c398d
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-05-28 22:12:05 -0400

    Time: 5 ms (95.91%), Space: 48.4 MB (80.01%) - LeetHub

---
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean searchMatrix(int[][] arr, int target) {
+        
+        int row = arr.length-1;
+        int col=0;
+        
+        while(row>=0 && col<arr[0].length) {
+            int curNum = arr[row][col];
+            if(curNum>target) {row--;} //upar jao if you are big; 
+            else if(curNum<target) {col++;} //left jao if you are small. 
+            else return true;
+        }
+        
+        return false;
+    }
+}
