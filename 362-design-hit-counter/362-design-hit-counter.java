e918db1edf5008ddaa407b03232d84b15e75f543
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-06-11 16:38:03 -0400

    Time: 1 ms (99.83%), Space: 42.3 MB (19.68%) - LeetHub

---
+++ 362-design-hit-counter/362-design-hit-counter.java
@@ -0,0 +1,28 @@
+class HitCounter {
+
+    ArrayDeque<Integer> q;
+    
+    public HitCounter() {
+        q = new ArrayDeque<>();
+    }
+    
+    public void hit(int timestamp) {
+        q.add(timestamp);
+        
+        int start = timestamp - 299;
+        while(!q.isEmpty() && q.peek()<start) {q.poll();}
+    }
+    
+    public int getHits(int timestamp) {
+        int start = timestamp - 299;
+        while(!q.isEmpty() && q.peek()<start) {q.poll();}
+        return q.size();
+    }
+}
+
+/**
+ * Your HitCounter object will be instantiated and called as such:
+ * HitCounter obj = new HitCounter();
+ * obj.hit(timestamp);
+ * int param_2 = obj.getHits(timestamp);
+ */
