5e7a9f92dd48f675a91889299ede03cffdf07a06
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-06-12 19:08:25 -0400

    Time: 3 ms (57.89%), Space: 42.9 MB (54.07%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.java
@@ -1,7 +1,7 @@
 class Solution {
     public int myAtoi(String str) {
     
-        if(str==null || str.isEmpty()) return 0;
+        if(str.isEmpty()) return 0;
         
         int sign=1, i=0, n=str.length();
         int max = Integer.MAX_VALUE;
@@ -11,15 +11,16 @@ class Solution {
         
         if(i>=n) {return 0;}
         
-        if(str.charAt(i)=='+' || str.charAt(i)=='-'){sign = str.charAt(i) == '+' ? 1 : -1; i++;}
+        if(str.charAt(i)=='+' || str.charAt(i)=='-') {
+            sign = str.charAt(i) == '+' ? 1 : -1; 
+            i++;
+        }
         
         long res = 0;
         
         while(i<n && Character.isDigit(str.charAt(i))) {
             res = res*10 + (str.charAt(i) - '0');
-            
             if(res*sign > max || res*sign < min) {return sign == 1 ? max : min;}
-            
             i++;
         }
         
