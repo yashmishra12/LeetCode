4a450e245f50364c275cd81b96778828dc9ae262
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-06-16 19:22:34 -0400

    Time: 1 ms (61.78%), Space: 42.1 MB (42.84%) - LeetHub

---
+++ 28-implement-strstr/28-implement-strstr.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        
+        int m = haystack.length();
+        int n = needle.length();
+        
+        if (n>m) {return -1;}
+        
+        for(int i=0; i<= m-n; i++) {
+            int j;
+            for(j=0; j<n; j++) {
+                if(haystack.charAt(i+j) != needle.charAt(j)) { break; }
+            }
+              if (j==n) {return i;}
+        }
+        
+      
+         return -1;
+    }
+}
