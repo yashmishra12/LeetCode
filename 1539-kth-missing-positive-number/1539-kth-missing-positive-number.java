96fae97e3be01a3ede671f2e3dbf56e30f3a9188
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-06-16 18:30:26 -0400

    Time: 0 ms (100.00%), Space: 43.3 MB (41.19%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
@@ -1,28 +1,53 @@
 class Solution {
     public int findKthPositive(int[] arr, int k) {
-        int expectedNum = 1;
+//         int expectedNum = 1;
         
-        for(int n: arr) {
+//         for(int n: arr) {
             
-            if (n != expectedNum) {
+//             if (n != expectedNum) {
                 
-               while(expectedNum != n) {
-                    k--;
-                    if(k==0) {return expectedNum;}
-                    expectedNum++;
-               }
-                expectedNum++;
+//                while(expectedNum != n) {
+//                     k--;
+//                     if(k==0) {return expectedNum;}
+//                     expectedNum++;
+//                }
+//                 expectedNum++;
+//             }
+//             else {
+//                 expectedNum++;
+//             }
+//         }
+        
+//         while(k>0) {
+//             expectedNum++;
+//             k--;
+//         }
+        
+//         return expectedNum-1;
+        
+        int n = arr.length;
+        int left = 0, right = arr.length - 1;
+        
+        int missing = compute(arr[n-1], n); //expected = index+1
+        
+        while(left<=right) {
+            int mid = left + (right-left)/2;
+            missing = compute(arr[mid], mid+1);
+            
+            if(missing >= k) { //no. lies on left
+                right = mid -1;
+            } else {
+                 left = mid+1;
             }
-            else {
-                expectedNum++;
-            }
+
         }
         
-        while(k>0) {
-            expectedNum++;
-            k--;
-        }
         
-        return expectedNum-1;
+        if (right==-1) {return k;}
+        return arr[right] + (k - compute(arr[right], right+1));
+    }
+    
+    public static int compute(int actual, int expected) {
+        return actual-expected;   
     }
 }
