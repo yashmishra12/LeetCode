b4158ef3680a5409d6085c6f2cd99bf9a7eb492a
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-06-17 13:51:55 -0400

    Time: 40 ms (52.05%), Space: 43.1 MB (92.73%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.java
+++ 875-koko-eating-bananas/875-koko-eating-bananas.java
@@ -1,11 +1,11 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
         
-        int left = 1, right = 1; 
-        for(int pile: piles) {
-            //max speed = max pile. Jaldi khake faida nahi hai because koko will wait till the remaining time
-            right = Math.max(right, pile); 
-        }
+        int left = 1, right = 1000000000; 
+        // for(int pile: piles) {
+        //     //max speed = max pile. Jaldi khake faida nahi hai because koko will wait till the remaining time
+        //     right = Math.max(right, pile); 
+        // }
         
         while(left < right) {
             int mid = (left+right)/2;
@@ -13,14 +13,11 @@ class Solution {
             
             for(int pile: piles) { 
                 hourSpent += Math.ceil((double) pile/mid);
-                if(hourSpent > h) {break;}
+                if (hourSpent > h) {break;}
             }
             
-            if(hourSpent <= h) {
-                right = mid;
-            } else {
-                left = mid + 1;
-            }
+            if(hourSpent <= h) {right = mid;} 
+            else { left = mid + 1; }
         }
         
         return right; 
