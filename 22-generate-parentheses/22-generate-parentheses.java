3bd9ad7846795b56f16cfaff4fd277c49c479319
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-06-16 20:01:03 -0400

    Time: 16 ms (8.19%), Space: 45.9 MB (6.06%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        
+        List<String> ans = new ArrayList<>();
+        
+        if (n==0) { 
+            ans.add("");
+            return ans;
+        } 
+        
+        for (int c=0; c<n; c++) {
+                for(String left: generateParenthesis(c)) {
+                    for (String right: generateParenthesis(n-1-c)) {
+                        ans.add("("+left+")"+right);
+                    }
+                }
+                
+            }
+        
+        return ans;
+        
+    
+    }
+}
