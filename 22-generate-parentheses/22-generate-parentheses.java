2ee3438a48fca72be7125c036abb2654e33b1c0e
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-06-16 20:41:14 -0400

    Time: 4 ms (25.05%), Space: 44.6 MB (12.63%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.java
+++ 22-generate-parentheses/22-generate-parentheses.java
@@ -1,28 +1,46 @@
 class Solution {
-    public List<String> generateParenthesis(int n) {
+//     public List<String> generateParenthesis(int n) {
         
-        List<String> ans = new ArrayList<>();
+//         List<String> ans = new ArrayList<>();
         
-        backtrack(ans, new StringBuilder(), 0, 0, n);
-        return ans;
-    }
+//         backtrack(ans, new StringBuilder(), 0, 0, n);
+//         return ans;
+//     }
     
-    public void backtrack(List<String> ans, StringBuilder cur, int open, int close, int max) {
-        if (cur.length() == max*2) {
-            ans.add(cur.toString());
+//     public void backtrack(List<String> ans, StringBuilder sb, int open, int close, int n) {
+//         if (sb.length() == n*2) {
+//             ans.add(sb.toString());
+//             return;
+//         }
+        
+//         if (open < n) {
+//             sb.append("(");
+//             backtrack(ans, sb, open+1, close, n);
+//             sb.deleteCharAt(sb.length()-1);
+//         }
+        
+//          if (close < open) {
+//             sb.append(")");
+//             backtrack(ans, sb, open, close+1, n);
+//             sb.deleteCharAt(sb.length()-1); // undoes sb.append("*") 
+//         }
+//     }
+        public List<String> generateParenthesis(int n) {
+        List<String> results = new ArrayList<>();
+        generateParenthesis(n, n, "", results);
+        return results;
+    }
+
+    public void generateParenthesis(int openCount, int closeCount, String result, List<String> results) {
+        if(closeCount < openCount) return;
+        if(openCount < 0) return;
+        if(openCount == 0 && closeCount == 0) {
+            results.add(result);
             return;
         }
-        
-        if (open < max) {
-            cur.append("(");
-            backtrack(ans, cur, open+1, close, max);
-            cur.deleteCharAt(cur.length()-1);
-        }
-        
-         if (close < open) {
-            cur.append(")");
-            backtrack(ans, cur, open, close+1, max);
-            cur.deleteCharAt(cur.length()-1);
-        }
+
+        generateParenthesis(openCount - 1, closeCount, result + "(", results);
+        generateParenthesis(openCount, closeCount - 1, result + ")", results);
     }
+
 }
