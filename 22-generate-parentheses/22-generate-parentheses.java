2168f5cb408a31dafc6aa822f9a1eb29370696b6
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-06-16 20:32:18 -0400

    Time: 1 ms (96.94%), Space: 43.7 MB (51.61%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.java
+++ 22-generate-parentheses/22-generate-parentheses.java
@@ -3,22 +3,26 @@ class Solution {
         
         List<String> ans = new ArrayList<>();
         
-        if (n==0) { 
-            ans.add("");
-            return ans;
-        } 
-        
-        for (int c=0; c<n; c++) {
-                for(String left: generateParenthesis(c)) {
-                    for (String right: generateParenthesis(n-1-c)) {
-                        ans.add("("+left+")"+right);
-                    }
-                }
-                
-            }
-        
+        backtrack(ans, new StringBuilder(), 0, 0, n);
         return ans;
-        
+    }
     
+    public void backtrack(List<String> ans, StringBuilder cur, int open, int close, int max) {
+        if (cur.length() == max*2) {
+            ans.add(cur.toString());
+            return;
+        }
+        
+        if (open < max) {
+            cur.append("(");
+            backtrack(ans, cur, open+1, close, max);
+            cur.deleteCharAt(cur.length()-1);
+        }
+        
+         if (close < open) {
+            cur.append(")");
+            backtrack(ans, cur, open, close+1, max);
+            cur.deleteCharAt(cur.length()-1);
+        }
     }
 }
