16cf836afb40b47382fc05ec0f7b185249523bf9
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-06-18 14:28:45 -0400

    Time: 3 ms (41.29%), Space: 43.8 MB (46.98%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.java
+++ 22-generate-parentheses/22-generate-parentheses.java
@@ -5,26 +5,25 @@ class Solution {
         
         List<String> ans = new ArrayList<>();
         
-        backtrack(ans, new StringBuilder(), 0, 0, n);
+        backtrack(ans, "", 0, 0, n);
         return ans;
     }
     
-    public void backtrack(List<String> ans, StringBuilder sb, int open, int close, int n) {
-        if (sb.length() == n*2) {
-            ans.add(sb.toString());
-            return;
-        }
+    public void backtrack(List<String> ans, String prefix, int open, int close, int n) {
         
         if (open < n) {
-            sb.append("(");
-            backtrack(ans, sb, open+1, close, n);
-            sb.deleteCharAt(sb.length()-1);
+            String newPrefix = prefix + "(";
+            backtrack(ans, newPrefix, open+1, close, n);
         }
         
          if (close < open) {
-            sb.append(")");
-            backtrack(ans, sb, open, close+1, n);
-            sb.deleteCharAt(sb.length()-1); // undoes sb.append("*") 
+            String newPrefix = prefix + ")";
+            backtrack(ans, newPrefix, open, close+1, n);
+        }
+        
+        if (close==n) {
+            ans.add(prefix);
+            return;
         }
     }
 
