5da43af41211de700dc15b0778287921af7006d6
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-06-18 13:30:35 -0400

    Time: 87 ms (28.70%), Space: 72.4 MB (28.69%) - LeetHub

---
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.java
@@ -0,0 +1,39 @@
+class Solution {
+    private final int MOD = (int)1e9+7;
+    
+    public int numberOfWays(String corridor) {
+        int count = 0;
+        
+        for(int i=0; i<corridor.length(); i++) {
+            if(corridor.charAt(i)=='S') { count++; }
+        }
+            
+        if(count==0 || count%2 != 0) {return 0;}
+            
+        int seatCount = 0;
+        long ans = 1;
+        
+        for(int i=0; i<corridor.length();i++) {
+            if (corridor.charAt(i)=='S') {seatCount++;}
+            
+            if(seatCount==2) {
+                int inCount = 1;
+                i++;
+                while(i<corridor.length() && corridor.charAt(i)=='P') { 
+                    inCount++;
+                    i++;
+                }
+                if (i==corridor.length()) {break;}
+
+                ans= (ans*inCount)%MOD;
+                seatCount=0;
+                i--;
+                }
+
+            }
+        
+        return (int)ans%MOD;
+        }
+            
+            
+}
