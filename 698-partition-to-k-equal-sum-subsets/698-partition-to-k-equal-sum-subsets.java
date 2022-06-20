4a0bb296b78c7d1b13e724ff8f1e1fcdb34edf50
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-06-20 16:27:28 -0400

    Time: 121 ms (60.53%), Space: 66.9 MB (9.66%) - LeetHub

---
+++ 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.java
@@ -0,0 +1,153 @@
+// class Solution {
+//     public boolean canPartitionKSubsets(int[] arr, int k) {
+//         int totalSum = 0;
+//         int n = arr.length;
+        
+//         if(k==1) {return true;}
+//         if(k>n) {return false;}
+        
+        
+//         for(int i=0; i<n; i++) {
+//             totalSum += arr[i];
+//         }
+        
+//         if(totalSum % k != 0) {
+//             return false;
+//         }
+        
+//         int targetSum = totalSum / k;
+//         boolean[] visited = new boolean[n];
+        
+//         Arrays.sort(arr);
+//         reverse(arr);
+        
+//         return backtrack(arr, 0,0, k, targetSum, visited);
+
+//     }
+    
+//     public boolean backtrack(int[] arr, int count, int curSum, int k, int targetSum, boolean[] visited) {
+//         int n = arr.length;
+        
+//         if(count == k-1) {return true;}
+        
+//         if(curSum > targetSum) {return false;}
+        
+//         if(curSum == targetSum) { //curSum becomes zero
+//             return backtrack(arr, count+1, 0, k, targetSum, visited);
+//         }
+        
+//         for(int i=0; i<n; i++) {
+//             if(visited[i] == false) {
+//                 visited[i]=true;
+//                 if (backtrack(arr, count, curSum+arr[i], k, targetSum, visited)) {
+//                     return true;
+//                 }
+//                 visited[i] = false;
+//             }
+//         }
+        
+//         return false;
+//     }
+    
+//      void reverse(int[] arr) {
+//         for (int i = 0, j = arr.length - 1; i < j; i++, j--) { 
+//             int temp = arr[i];
+//             arr[i] = arr[j];
+//             arr[j] = temp;
+//         }
+//     }
+  
+    
+// }
+
+class Solution {
+    private boolean backtrack(int[] arr, int index, int count, int currSum, int k, 
+                              int targetSum, char[] taken, HashMap<String, Boolean> memo) {
+
+        int n = arr.length;
+      
+        // We made k - 1 subsets with target sum and last subset will also have target sum.
+        if (count == k - 1) { 
+            return true;
+        }
+        
+        // No need to proceed further.
+        if (currSum > targetSum) { 
+            return false;
+        }
+        
+        String takenStr = new String(taken);
+        
+        // If we have already computed the current combination.
+        if (memo.containsKey(takenStr)) {
+            return memo.get(takenStr);
+        }
+      
+        // When curr sum reaches target then one subset is made.
+        // Increment count and reset current sum.
+        if (currSum == targetSum) {
+            boolean ans = backtrack(arr, 0, count + 1, 0, k, targetSum, taken, memo);
+            memo.put(takenStr, ans);
+            return ans;
+        }
+        
+        // Try not picked elements to make some combinations.
+        for (int j = index; j < n; ++j) {
+            if (taken[j] == '0') {
+                // Include this element in current subset.
+                taken[j] = '1';
+                
+                // If using current jth element in this subset leads to make all valid subsets.
+                if (backtrack(arr, j + 1, count, currSum + arr[j], k, targetSum, taken, memo)) {
+                    return true;
+                }
+                
+                // Backtrack step.
+                taken[j] = '0';
+            }
+        } 
+      
+        // We were not able to make a valid combination after picking each element from array,
+        // hence we can't make k subsets.
+        memo.put(takenStr, false);
+        return false;
+    }
+    
+    void reverse(int[] arr) {
+        for (int i = 0, j = arr.length - 1; i < j; i++, j--) { 
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+  
+    public boolean canPartitionKSubsets(int[] arr, int k) {
+        int totalArraySum = 0;
+        int n = arr.length;
+        
+        for (int i = 0; i < n; ++i) {
+             totalArraySum += arr[i];
+        }
+      
+        // If total sum not divisible by k, we can't make subsets.
+        if (totalArraySum % k != 0) { 
+            return false;
+        }
+      
+        // Sort in decreasing order.
+        Arrays.sort(arr);
+        reverse(arr);
+        
+        int targetSum = totalArraySum / k;
+        
+        char[] taken = new char[n];
+        for(int i = 0; i < n; ++i) {
+            taken[i] = '0';
+        }
+        
+        // Memoize the ans using taken element's string as key.
+        HashMap<String, Boolean> memo = new HashMap<>();
+      
+        return backtrack(arr, 0, 0, 0, k, targetSum, taken, memo);
+    }
+}
