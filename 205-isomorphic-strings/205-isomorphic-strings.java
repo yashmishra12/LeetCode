71c700090f48adb249def41168e846074705e359
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-06-22 01:10:35 -0400

    Time: 5 ms (90.39%), Space: 43.1 MB (40.24%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.java
+++ 205-isomorphic-strings/205-isomorphic-strings.java
@@ -3,15 +3,22 @@ class Solution {
         
         int[] m1 = new int[128];
         int[] m2 = new int[128];
+        Arrays.fill(m1, -1);
+        Arrays.fill(m2, -1);
+        
         int n = s.length();
         
         for(int i=0; i<n; i++) {
             char c1 = s.charAt(i);
             char c2 = t.charAt(i);
             
-            if(m1[c1] != m2[c2]) {return false;}
-            m1[c1] = i+1;
-            m2[c2] = i+1;
+            if(m1[c1] == -1 && m2[c2]==-1) {
+                            m1[c1] = c2;
+            m2[c2] = c1;
+            }
+            
+            if(m1[c1] != c2 && m2[c2] != c1) {return false;}
+
         }
         
         return true;
