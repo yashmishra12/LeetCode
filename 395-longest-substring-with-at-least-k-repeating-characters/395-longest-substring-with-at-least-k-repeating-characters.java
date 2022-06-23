036bf689665bb7748934b1d4ef9234a67e614614
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-06-23 18:10:16 -0400

    Time: 1 ms (90.58%), Space: 41.9 MB (68.34%) - LeetHub

--- 395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.java
+++ 395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.java
@@ -12,7 +12,7 @@ class Solution {
         boolean  valid = true;
         for(int end=0; end< s.length(); end++) {
             char curChar = s.charAt(end);
-            if (freq[curChar-'a'] > 0 && freq[curChar-'a']<k) {
+            if (freq[curChar-'a']<k) {
                 String sb = s.substring(start, end);
                 maxLen = Math.max(maxLen, longestSubstring(sb, k));
                 start = end+1;
