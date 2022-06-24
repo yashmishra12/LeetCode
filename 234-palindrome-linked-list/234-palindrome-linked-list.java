01811ec8f0c4054f69c88485831be626c807f3c8
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-06-24 18:36:09 -0400

    Time: 5 ms (88.33%), Space: 59.2 MB (84.98%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.java
+++ 234-palindrome-linked-list/234-palindrome-linked-list.java
@@ -11,8 +11,8 @@ class Solution {
         // Check whether or not there is a palindrome.
         ListNode p1 = head;
         ListNode p2 = secondHalfStart;
-        boolean result = true;
-        while (result && p2 != null) {
+
+        while (p2 != null) {
             if (p1.val != p2.val) {return false;}
             p1 = p1.next;
             p2 = p2.next;
