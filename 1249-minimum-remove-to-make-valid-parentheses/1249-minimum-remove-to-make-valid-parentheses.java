8ee4639c89630121de40c1a66c0a522aa963db2b
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-09-08 23:12:28 -0400

    Time: 55 ms (29.06%), Space: 54.7 MB (18.78%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -6,24 +6,25 @@ class Solution {
         
         for(int i=0; i<s.length(); i++) {
             if (s.charAt(i) == '(') {
-                st.push(i);
+                st.push(i); //index of opening braces added
             }
             else if (s.charAt(i)== ')') {
                 if (st.isEmpty()) {
-                    indxToRemove.add(i);
+                    indxToRemove.add(i); //close braces with no open braces...IMBALANCE -> add index
                 } else {
-                    st.pop();
+                    st.pop(); //the close braces consumed the open braces. 
                 }
             }
         }
         
+         //LeftOver open braces index added
         while(!st.isEmpty()) {
             indxToRemove.add(st.pop());
         }
         
         StringBuilder sb = new StringBuilder();
         for(int i=0; i<s.length();i++) {
-            if(indxToRemove.contains(i)==false) {
+            if(indxToRemove.contains(i) == false) {
                 sb.append(s.charAt(i));
             }
         }
