bbf92cf775d643c0315ba3e2bde1604a57894e53
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-09-14 17:00:42 -0400

    Time: 124 ms (5.25%), Space: 53.1 MB (11.93%) - LeetHub

---
+++ 636-exclusive-time-of-functions/636-exclusive-time-of-functions.java
@@ -0,0 +1,48 @@
+class Solution {
+   public int[] exclusiveTime(int n, List<String> logs) {
+        Stack<Execution> callStack = new Stack<>();
+        for (String log : logs) {
+            callStack.push(new Execution(log));
+        }
+
+        int[] solution = new int[n];
+
+        Stack<Execution> backgroundExecution = new Stack<>();
+
+        while (!callStack.isEmpty()) {
+            Execution executing = callStack.pop();
+            if (callStack.peek().isEnd) {
+                //send the current execution to background because something executed between its start and end
+                backgroundExecution.push(executing);
+            } else {
+                //execution Stack will be a start, so we calculate how long it took
+                int executionTime = 1 + (executing.time - callStack.pop().time);
+                //update solution after processing
+                solution[executing.id] += executionTime;
+                //clean the background and update the time removing the subprocess execution time
+                while (!backgroundExecution.isEmpty()) {
+                    Execution toExecute = backgroundExecution.pop();
+                    toExecute.time -= executionTime;
+                    callStack.push(toExecute);
+                }
+            }
+        }
+
+        return solution;
+    }
+
+    static class Execution {
+        int id;
+        boolean isEnd;
+        int time;
+
+        public Execution(String log) {
+            String[] execution = log.split(":");
+            this.id = Integer.parseInt(execution[0]);
+            this.isEnd = execution[1].equals("end");
+            this.time = Integer.parseInt(execution[2]);
+        }
+
+    }
+    
+}
