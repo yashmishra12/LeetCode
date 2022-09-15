903f4bcf33584d8c725fe098e8869a0340a3d671
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-09-15 17:05:53 -0400

    Time: 138 ms (5.24%), Space: 52.5 MB (29.71%) - LeetHub

--- 636-exclusive-time-of-functions/636-exclusive-time-of-functions.java
+++ 636-exclusive-time-of-functions/636-exclusive-time-of-functions.java
@@ -1,5 +1,12 @@
+import java.util.*;
+
 class Solution {
-   public int[] exclusiveTime(int n, List<String> logs) {
+    public static void main(String[] args) {
+    ArrayList<String> arr = new ArrayList<>(Arrays.asList("0:start:0","1:start:2","1:end:5","0:end:6"));
+        System.out.println(Arrays.toString(exclusiveTime(2, arr)));
+        
+    }
+   public static int[] exclusiveTime(int n, List<String> logs) {
         Stack<Execution> callStack = new Stack<>();
         for (String log : logs) {
             callStack.push(new Execution(log));
@@ -11,10 +18,13 @@ class Solution {
 
         while (!callStack.isEmpty()) {
             Execution executing = callStack.pop();
-            if (callStack.peek().isEnd) {
+            
+            if (callStack.peek().isEnd) { // you have popped, but uske neeche wala (current peak) is also END
                 //send the current execution to background because something executed between its start and end
                 backgroundExecution.push(executing);
-            } else {
+            } 
+            
+            else {
                 //execution Stack will be a start, so we calculate how long it took
                 int executionTime = 1 + (executing.time - callStack.pop().time);
                 //update solution after processing
@@ -22,7 +32,7 @@ class Solution {
                 //clean the background and update the time removing the subprocess execution time
                 while (!backgroundExecution.isEmpty()) {
                     Execution toExecute = backgroundExecution.pop();
-                    toExecute.time -= executionTime;
+                    toExecute.time -= executionTime; // here you tell the process below to delete the time for the above process
                     callStack.push(toExecute);
                 }
             }
