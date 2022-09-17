9328cb41466f1ceda0ed7275d8701b8ca5f92fd2
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-09-17 16:34:47 -0400

    Time: 0 ms (100.00%), Space: 41.7 MB (78.14%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.java
+++ 1328-break-a-palindrome/1328-break-a-palindrome.java
@@ -1,32 +1,51 @@
+// class Solution {
+//     public String breakPalindrome(String palindrome) {
+//         if(palindrome.length()==1){ return ""; }
+        
+//         char[] arr = palindrome.toCharArray();
+//         boolean flag = false;
+        
+//         for(int i=0; i<arr.length/2; i++) {
+//             char curChar = arr[i];
+//             if (curChar != 'a') {
+//                 arr[i]='a';
+//                 flag=true;
+//                 break;
+//             }
+//         }
+        
+//         StringBuilder sb = new StringBuilder();
+
+//         for(char c: arr) { sb.append(c); }
+        
+//         if (flag==true) {
+//             return sb.toString();
+//         }
+//         else {
+//             arr[arr.length-1]='b';
+//             sb.replace(arr.length-1,arr.length, "b");
+//             return sb.toString();
+//         }
+//     }
+// }
+
 class Solution {
     public String breakPalindrome(String palindrome) {
-        if(palindrome.length()==1){ return ""; }
+        int length = palindrome.length();
+        if (length == 1) { 
+            return "";
+        }
+        // Strings are immutable in Java, convert it into a char array
+        char[] palindromeArray = palindrome.toCharArray();
         
-        char[] arr = palindrome.toCharArray();
-        boolean flag = false;
-        
-        for(int i=0; i<arr.length/2; i++) {
-            char curChar = arr[i];
-            if (curChar != 'a') {
-                arr[i]='a';
-                flag=true;
-                break;
+        for (int i = 0; i < length / 2; i++) {
+            if (palindromeArray[i] != 'a') {
+                palindromeArray[i] = 'a';
+                return String.valueOf(palindromeArray);
             }
         }
         
-        StringBuilder sb = new StringBuilder();
-
- for(char c: arr) { sb.append(c); }
-        
-        if (flag==true) {
-                
-           
-            return sb.toString();
-        }
-        else {
-            arr[arr.length-1]='b';
-            sb.replace(arr.length-1,arr.length, "b");
-            return sb.toString();
-        }
+        palindromeArray[length - 1] = 'b';
+        return String.valueOf(palindromeArray);
     }
 }
