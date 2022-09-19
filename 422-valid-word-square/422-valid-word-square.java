870fee7e553ecad41cb0cbfaca1bbb4bde9eaaa0
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-09-19 14:52:30 -0400

    Time: 22 ms (30.92%), Space: 42.4 MB (75.50%) - LeetHub

--- 422-valid-word-square/422-valid-word-square.java
+++ 422-valid-word-square/422-valid-word-square.java
@@ -3,14 +3,13 @@ class Solution {
 	
        int k = 0;
        int n = words.size();
+       
       for (int i = 0; i < n; i++) {
         for (int j = 0; j < words.get(i).length(); j++) {
-            if (j >= n || 
-                k >= words.get(j).length() || 
-				words.get(i).charAt(j) != words.get(j).charAt(k)
-               ) {
-                return false;
-            }
+            // char c1 = words.get(i).charAt(j);
+            // char c2 = words.get(j).charAt(k);
+            if (j >= n || k >= words.get(j).length() || words.get(i).charAt(j)!=words.get(j).charAt(k)) 
+                {return false;}
         }
           k++;
       }
