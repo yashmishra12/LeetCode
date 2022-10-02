f91aaca6e077b49358f4a6e8438b3b84509ab062
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-10-02 01:11:48 -0400

    Time: 0 ms (100.00%), Space: 38.6 MB (99.88%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -2,13 +2,12 @@ class Solution {
     public int fib(int n) {
         
         if(n==0 || n==1) {return n;}
-        int[] arr = new int[n+1];
        
         int prev1 = 0;
         int prev2 = 1;
         int prev3 = 0;
         
-        for(int i=2; i<arr.length; i++) {
+        for(int i=2; i<n+1; i++) {
             prev3 = prev2+prev1;
             prev1 = prev2;
             prev2 = prev3;
