16b3a2347c0d9b52ccf0ac5a5792bfe9c0477c68
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-10-02 04:44:05 -0400

    Time: 16 ms (92.07%), Space: 40.9 MB (95.84%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
@@ -6,30 +6,51 @@ class Solution {
     
     public int numRollsToTarget(int n, int k, int target) {
         Integer[][] memo = new Integer[n+1][target+1];
-        return helper(memo, 0, n, target, k);
+        // return helper(memo, 0, n, target, k);
+         return solver(memo, n, k, target);
+        
     }
     
-    int helper(Integer[][] memo, int diceIndex, int n, int target, int k) {
-        if (diceIndex==n) {
-            return target==0 ? 1:0;
+    int solver(Integer[][] memo, int dice, int face, int target) {
+        
+        if (target<0) {return 0;}
+        if (dice ==0 && target != 0) {return 0;}
+        if (dice==0 && target==0) {return 1;}
+        
+        if(memo[dice][target] != null) {
+           return memo[dice][target]; 
         }
         
-
-        if(diceIndex !=0 && target==0) {return 0;}
-        if(target<0) {return 0;}
-        
-        if(memo[diceIndex][target] != null) {
-           return memo[diceIndex][target]; 
+        int ans = 0;
+        for(int i=1; i<=face; i++) {
+            ans = (ans + solver(memo, dice-1, face, target-i))%MOD;
         }
-        
-        int ways = 0;
-        
-        for(int i=1; i<=k; i++) {
-            ways = (ways+helper(memo, diceIndex+1, n, target-i, k))%MOD;
-        }
-        memo[diceIndex][target] = ways;
-        
-        return ways;
+        memo[dice][target] = ans;
+        return ans;
         
     }
+    
+//     int helper(Integer[][] memo, int diceIndex, int n, int target, int k) {
+//         if (diceIndex==n) {
+//             return target==0 ? 1:0;
+//         }
+        
+
+//         if(diceIndex !=0 && target==0) {return 0;}
+//         if(target<0) {return 0;}
+        
+//         if(memo[diceIndex][target] != null) {
+//            return memo[diceIndex][target]; 
+//         }
+        
+//         int ways = 0;
+        
+//         for(int i=1; i<=k; i++) {
+//             ways = (ways+helper(memo, diceIndex+1, n, target-i, k))%MOD;
+//         }
+//         memo[diceIndex][target] = ways;
+        
+//         return ways;
+        
+//     }
 }
