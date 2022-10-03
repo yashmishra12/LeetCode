d79e404120669ebfda5ca6b1815d6c9cb36e9870
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-10-02 23:19:30 -0400

    Time: 11 ms (79.00%), Space: 89.8 MB (53.02%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.java
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.java
@@ -1,16 +1,16 @@
 class Solution {
     public int minCost(String s, int[] neededTime) {
-        int n = neededTime.length, groupSum = neededTime[0], groupMax = neededTime[0], ans = 0;
+        int ans = 0;
         
    
-        for(int i=1; i<n; i++) {
+        for(int i=1; i<neededTime.length; i++) {
             if (s.charAt(i)==s.charAt(i-1)) {
                 if (neededTime[i]>neededTime[i-1]) {
-                    ans += neededTime[i-1];
-                    
+                    ans += neededTime[i-1]; //consume the minimum
+                    //no need to shift as max is already at right
                 }else {
-                    ans += neededTime[i];
-                    neededTime[i] = neededTime[i-1];
+                    ans += neededTime[i]; //consume the minimum
+                    neededTime[i] = neededTime[i-1]; //shifting max to right
                 }
             }
         }
