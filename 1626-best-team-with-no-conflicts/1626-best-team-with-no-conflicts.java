7a9a50fe9448c684e0d09b620a4c272c46ee5246
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-10-14 18:59:15 -0400

    Time: 48 ms (88.71%), Space: 42.5 MB (87.63%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.java
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.java
@@ -11,7 +11,9 @@ class Solution {
     }
     
     public int bestTeamScore(int[] scores, int[] ages) {
+
         int n = scores.length;
+        if(n==1) {return scores[0];}
         Player[] players = new Player[n];
         for(int i = 0; i < n; i++) {
             players[i] = new Player(scores[i], ages[i]);
@@ -20,8 +22,9 @@ class Solution {
         Arrays.sort(players, (a,b) -> a.age == b.age ? a.score - b.score : a.age - b.age);
         
         int[] dp = new int[n];
+        dp[0] = players[0].score;
         int max = 0;
-        for(int i = 0; i < n; i++) {
+        for(int i = 1; i < n; i++) {
             dp[i] = players[i].score;
             for(int j = 0; j < i; j++) {
                 if(players[j].age <= players[i].age && players[j].score <= players[i].score) {
