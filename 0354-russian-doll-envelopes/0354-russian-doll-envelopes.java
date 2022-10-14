f142a337cbc92c0df4677aa503671cdc905105ba
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-10-14 18:06:39 -0400

    Time: 70 ms (75.62%), Space: 85 MB (26.94%) - LeetHub

--- 0354-russian-doll-envelopes/0354-russian-doll-envelopes.java
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.java
@@ -12,10 +12,10 @@ class Solution {
         for(int i=1; i<envelopes.length; i++) {
             int idx = binarySearch(dp, envelopes[i][1]);
 // index of the search key, if it is in the array; otherwise, (-(insertion point) – 1)
-            if(idx<0) {
-                    idx += 1;
-                    idx *= -1;
-                }
+            // if(idx<0) {
+            //         idx += 1;
+            //         idx *= -1;
+            //     }
 
             if(idx<dp.size()) {
                  dp.set(idx, envelopes[i][1]);
