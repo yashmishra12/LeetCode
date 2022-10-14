7cb367e7b45c153b09f6850c77b4b84cbdc88a85
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-10-14 18:12:06 -0400

    Time: 147 ms (6.08%), Space: 83.2 MB (32.14%) - LeetHub

--- 0354-russian-doll-envelopes/0354-russian-doll-envelopes.java
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.java
@@ -1,40 +1,34 @@
 class Solution {
     public int maxEnvelopes(int[][] envelopes) {
-        // Width in increasing order
-        // height in decreasing order
+        // Width in increasing order, height in decreasing order
+        //our search finds the last presence of num. Hence, smaller height remains at end
         Arrays.sort(envelopes, (a,b) -> a[0]-b[0] == 0 ? b[1]-a[1]:a[0]-b[0]);
-        
-
         ArrayList<Integer> dp = new ArrayList<>();
 
         dp.add(envelopes[0][1]);
         
         for(int i=1; i<envelopes.length; i++) {
-            int idx = binarySearch(dp, envelopes[i][1]);
-// index of the search key, if it is in the array; otherwise, (-(insertion point) – 1)
-            // if(idx<0) {
-            //         idx += 1;
-            //         idx *= -1;
-            //     }
+            int idx = Collections.binarySearch(dp, envelopes[i][1]);
 
+            if(idx<0) {
+                idx += 1;
+                idx *= -1;
+            }
+            
             if(idx<dp.size()) {
                  dp.set(idx, envelopes[i][1]);
             }
             else {
                 dp.add(envelopes[i][1]);
             }
-           
-
         }
         return dp.size();
     }
     
-        public static int binarySearch(ArrayList<Integer> seq, int key) {
-        
+    public static int binarySearch(ArrayList<Integer> seq, int key) {    
         int lo = 0, hi = seq.size()-1;
         while(lo<=hi){
             int mid = lo+(hi-lo)/2;
-
             if(seq.get(mid)<key){
                 lo = mid+1;
             } else {
