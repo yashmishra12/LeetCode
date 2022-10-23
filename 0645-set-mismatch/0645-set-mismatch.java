07fd4bedf64ddcc4a98d36dfb56f468d4dad4f33
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-10-22 20:19:25 -0400

    Time: 57 ms (5.38%), Space: 55.9 MB (12.12%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -2,15 +2,20 @@ class Solution {
     public int[] findErrorNums(int[] nums) {
         HashSet<Integer> hs = new HashSet<>();
         int[] arr = new int[2];
-        for(int a: nums) {
+        // for(int a: nums) {
+        //     if(hs.add(a)==false){
+        //         arr[0] = a;
+        //     }
+        // }
+        Arrays.sort(nums);
+        for(int i=1;i<=nums.length; i++) {
+            int a = nums[i-1];
             if(hs.add(a)==false){
                 arr[0] = a;
             }
-        }
-        for(int i=1;i<=nums.length; i++) {
             if (!hs.contains(i)){
                 arr[1]=i;
-                break;
+                // break;
             }
         }
         return arr;
