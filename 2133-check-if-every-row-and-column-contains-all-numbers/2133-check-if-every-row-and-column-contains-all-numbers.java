290a502012e643dcc91d072dca4b9bf51ceec6b4
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-10-23 00:42:23 -0400

    Time: 13 ms (81.76%), Space: 67.5 MB (81.01%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.java
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.java
@@ -4,21 +4,28 @@ class Solution {
         
         int len = matrix.length;
         boolean[] m;
+                boolean[] m2;
         
-        for(int[] i : matrix){
-            m = new boolean[len+1];
-            for(int j : i){
-                if(m[j])
-                    return false;
-                m[j] = true;
-            }
-        }
+        // for(int[] i : matrix){
+        //     m = new boolean[len+1];
+        //     for(int j : i){
+        //         if(m[j]){return false;}
+        //         m[j] = true;
+        //     }
+        // }
         
         for(int i = 0; i < len; i++){
+            
+            m2 = new boolean[len+1];
+            int[] i2 = matrix[i];
+            for(int j : i2){
+                if(m2[j]){return false;}
+                m2[j] = true;
+            }
+            
             m = new boolean[len+1];
             for(int j = 0; j < len; j++){
-                if(m[matrix[j][i]])
-                    return false;
+                if(m[matrix[j][i]]){return false;}
                 m[matrix[j][i]] = true;
             }
         }
