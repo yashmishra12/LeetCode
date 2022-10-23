5c5f2f21d61c854fd561ce31ad4aa9fb3d47caf4
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-10-23 00:38:55 -0400

    Time: 13 ms (81.76%), Space: 73 MB (58.85%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.java
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.java
@@ -1,47 +1,29 @@
 class Solution {
     public boolean checkValid(int[][] matrix) {
-//         for(int i=0; i<matrix.length;i++) {
-//             ArrayList<Integer> row = new ArrayList<>();
-//             for(int n: matrix[i]) {
-//                 row.add(n);
-//             }
-//             if(!check(row)) {return false;}
-//             ArrayList<Integer> col = new ArrayList<>();
-            
-//             for(int j=0; j<matrix[i].length;j++) {
-//                 col.add(matrix[i][j]);
-//             }
-//              if(!check(col)) {return false;}
-//         }
-        
-//         return true;
-        
-        int n = matrix.length;
 
-    for(int r = 0; r < n; r ++){
-// rvis = row visited
-// cvis = coloum visited;
-
-        boolean rvis[] = new boolean[n + 1]; // row wise
-        boolean cvis[] = new boolean[n + 1];//col wise
         
-        for(int c = 0; c < n; c ++){
-            int rval = matrix[r][c]; //row wise
-            int cval = matrix[c][r]; //col wise
-            
-            if(rvis[rval] == true || cvis[cval] == true){
-// Element occurs more than once;
-
-                return false; // Matrix is not valid;
+        int len = matrix.length;
+        boolean[] m;
+        
+        for(int[] i : matrix){
+            m = new boolean[len+1];
+            for(int j : i){
+                if(m[j])
+                    return false;
+                m[j] = true;
             }
-         
-            rvis[rval] = true;
-        cvis[cval] = true;
+        }
+        
+        for(int i = 0; i < len; i++){
+            m = new boolean[len+1];
+            for(int j = 0; j < len; j++){
+                if(m[matrix[j][i]])
+                    return false;
+                m[matrix[j][i]] = true;
+            }
+        }
+        
+        return true;
     }
-}
- 
-    return true; // Matrix valid;
-    }
-    
 
 }
