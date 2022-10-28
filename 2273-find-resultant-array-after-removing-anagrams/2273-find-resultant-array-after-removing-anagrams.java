3037437f3708d707241ed9e0665ec71af738b89e
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-10-27 23:17:05 -0400

    Time: 23 ms (8.21%), Space: 49.9 MB (5.93%) - LeetHub

---
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.java
@@ -0,0 +1,50 @@
+class Solution {
+    public List<String> removeAnagrams(String[] words) {
+        List<String> ans = new ArrayList<>();
+        int start = 0;
+        
+        if(words.length==1){
+            return new ArrayList<String>(Arrays.asList(words[0]));
+        }
+        
+        
+        for(int i=1; i<words.length-1; i++) {
+            String freqStart = freq(words[start]);
+            String freqI = freq(words[i]);
+            
+            if( freqStart.equals(freqI) ) {
+                // i++;
+            }else {
+                ans.add(words[start]);
+                start = i;
+                // i++;
+            }
+        }
+        
+        ans.add(words[start]);
+        
+        String freqStart = freq(words[start]);
+        String freqEnd = freq(words[words.length-1]);
+        
+        if(!freqStart.equals(freqEnd)) {
+				ans.add(words[words.length-1]);
+			}
+        
+        return ans;
+    }
+    
+    
+    private String freq(String word){
+		int[] freq = new int[26];
+		for(char c: word.toCharArray()){
+			freq[c-'a']++;
+		}
+
+		StringBuilder ans = new StringBuilder();
+		for(int el: freq){
+			ans.append(el); 
+		}
+
+		return ans.toString();
+	}
+}
