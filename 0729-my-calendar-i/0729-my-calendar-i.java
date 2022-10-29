4737d49bda105398c522710832a0ee13b9159774
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-10-29 00:52:28 -0400

    Time: 34 ms (80.88%), Space: 54 MB (77.78%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.java
+++ 0729-my-calendar-i/0729-my-calendar-i.java
@@ -7,10 +7,10 @@ class MyCalendar {
     }
     
     public boolean book(int start, int end) {
-        Integer prev = calendar.floorKey(start);
-        Integer next = calendar.ceilingKey(start);
+        Integer prevStart = calendar.floorKey(start);
+        Integer nextStart = calendar.ceilingKey(start);
         
-        if ((prev==null || calendar.get(prev)<=start) && (next==null || end<=next)) {
+        if ((prevStart==null || calendar.get(prevStart)<=start) && (nextStart==null || end<=nextStart)) {
             calendar.put(start, end);
             return true;
         }
