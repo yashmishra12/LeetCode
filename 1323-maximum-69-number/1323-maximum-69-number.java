bfd7a8a84379a8126a414447c464b6a417929f2c
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-11-06 20:09:23 -0500

    Time: 2 ms (48.46%), Space: 40.8 MB (61.22%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.java
+++ 1323-maximum-69-number/1323-maximum-69-number.java
@@ -1,7 +1,7 @@
 class Solution {
     public int maximum69Number (int num) {
-        String s = String.valueOf(num);
-        StringBuilder sb = new StringBuilder(s);
+
+        StringBuilder sb = new StringBuilder(String.valueOf(num));
         int i = 0;
         while(i<sb.length() && sb.charAt(i)=='9'){
             i++;
@@ -9,14 +9,8 @@ class Solution {
         if(i==sb.length()){
             return num;
         }
-        
-//         int j = sb.length()-1;
-//         while(j>=0 && sb.charAt(j)=='6'){
-//             j--;
-//         }
-        
+
         sb.setCharAt(i, '9');
-        // sb.setCharAt(j, '6');
         
         int res = Integer.parseInt(sb.toString());
         return res; 
