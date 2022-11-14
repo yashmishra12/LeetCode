753e8bae332f3a5e3eed877322e2d43dc0b7a2d6
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-11-14 17:56:17 -0500

    Time: 70 ms (30.20%), Space: 54.9 MB (9.04%) - LeetHub

--- 947-most-stones-removed-with-same-row-or-column/947-most-stones-removed-with-same-row-or-column.java
+++ 947-most-stones-removed-with-same-row-or-column/947-most-stones-removed-with-same-row-or-column.java
@@ -1,122 +1,68 @@
-// class Solution {
-//     class Points{
-//         int x;
-//         int y;
-//         Points(int x, int y){
-//             this.x = x;
-//             this.y = y;
-//         }
-//         public String toString(){
-//             return x+":"+y;
-//         }
-//     }
-//     public int removeStones(int[][] stones) {
-//         Points[] arr = new Points[stones.length];
-//         HashMap<Integer, ArrayList<String>> hmx = new HashMap<>();
-//         HashMap<Integer, ArrayList<String>> hmy = new HashMap<>();
-        
-//         for(int i=0; i<arr.length; i++){
-//             int x = stones[i][0];
-//             int y = stones[i][1];
-//             String coord = x+":"+y;
-            
-//             if(hmx.containsKey(x)){
-//                 hmx.get(x).add(coord);
-//             }else {
-//                 ArrayList<String> tmp = new ArrayList<>();
-//                 tmp.add(coord);
-//                 hmx.put(x, tmp);
-//             }
-            
-//             if(hmy.containsKey(y)){
-//                 hmy.get(y).add(coord);
-//             }else {
-//                 ArrayList<String> tmp = new ArrayList<>();
-//                 tmp.add(coord);
-//                 hmy.put(y, tmp);
-//             }
-            
-//             Points p = new Points(x, y);
-            
-//             arr[i] = p;
-//         }
-        
-//         Arrays.sort(arr, (a,b)->(a.x!=b.x ? b.x-a.x : b.y-a.y));
-        
-//         int answer = 0;
-        
-//         for(Points p: arr){
-//             int x = p.x;
-//             int y = p.y;
-//             String coord = x+":"+y;
-            
-//             if(hmx.get(x).size()>1 || hmy.get(y).size()>1){
-//                 answer++;
-//                 hmx.get(x).remove(coord);
-//                 hmy.get(y).remove(coord);
-//             }
-            
-//         }
-        
-//         return answer;
-//     }
-// }
 
 class Solution {
-    // Return true if stone a and b shares row or column
-    boolean shareSameRowOrColumn(int[] a, int[] b) {
-        return a[0] == b[0] || a[1] == b[1];
-    }
-    
-    // Returns the representative of vertex x
-    int find(int[] rep, int x) {
-        if (x == rep[x]) {
-            return x;
+    class DisjointSet {
+    List<Integer> rank = new ArrayList<>();
+    List<Integer> parent = new ArrayList<>();
+    List<Integer> size = new ArrayList<>(); 
+    public DisjointSet(int n) {
+        for(int i = 0;i<=n;i++) {
+            rank.add(0); 
+            parent.add(i); 
+            size.add(1); 
         }
-        // Uses Path compression
-        return rep[x] = find(rep, rep[x]);
     }
-    
-    // Combine the stone x and y, and returns 1 if they were not connected
-    int performUnion(int [] rep, int [] size, int x, int y) {
-        x = find(rep, x);
-        y = find(rep, y);
-        
-        if (x == y) {
-            return 0;
+
+    public int findUPar(int node) {
+        if(node == parent.get(node)) {
+            return node; 
         }
-        
-        if (size[x] > size[y]) {
-            rep[y] = x;
-            size[x] += size[y];
-        } else {
-            rep[x] = y;
-            size[y] += size[x];
-        }
-        
-        return 1;
+        int ulp = findUPar(parent.get(node)); 
+        parent.set(node, ulp); 
+        return parent.get(node); 
     }
+
     
+    public void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u); 
+        int ulp_v = findUPar(v); 
+        if(ulp_u == ulp_v) return; 
+        if(size.get(ulp_u) < size.get(ulp_v)) {
+            parent.set(ulp_u, ulp_v); 
+            size.set(ulp_v, size.get(ulp_v) + size.get(ulp_u)); 
+        }
+        else {
+            parent.set(ulp_v, ulp_u); 
+            size.set(ulp_u, size.get(ulp_u) + size.get(ulp_v));
+        }
+    }
+}
     public int removeStones(int[][] stones) {
-        int[] rep = new int[stones.length];
-        int[] size = new int[stones.length];
-        // Initialize rep to itself and size as 1
-        for (int i = 0; i < stones.length; i++) {
-            rep[i] = i;
-            size[i] = 1;
+        int maxRow = 0;
+        int maxCol = 0;
+        int n = stones.length;
+        for(int i=0; i<n; i++){
+            maxRow = Math.max(maxRow, stones[i][0]);
+            maxCol = Math.max(maxCol, stones[i][1]);
         }
         
-        int componentCount = stones.length;
-        for (int i = 0; i < stones.length; i++) {
-            for (int j = i + 1; j < stones.length; j++) {
-                if (shareSameRowOrColumn(stones[i], stones[j])) {
-                    // Decrement the componenets if union invloved merging
-                    componentCount -= performUnion(rep, size, i, j);
-                }
+        DisjointSet ds = new DisjointSet(maxRow+maxCol+1);
+        HashMap<Integer, Integer> stoneNodes = new HashMap<>();
+        
+        for(int i=0; i<n; i++) {
+            int nodeRow = stones[i][0];
+            int nodeCol = stones[i][1] + maxRow + 1;
+            ds.unionBySize(nodeRow, nodeCol);
+            stoneNodes.put(nodeRow, 1);
+            stoneNodes.put(nodeCol, 1);
+        }
+        
+        int count = 0;
+        for(Map.Entry<Integer, Integer> it: stoneNodes.entrySet()){
+            if(ds.findUPar(it.getKey()) == it.getKey()){
+                count++;
             }
         }
         
-        // Return the maximum stone that can be removed
-        return stones.length - componentCount;
+        return stones.length - count;
     }
-};
+}
