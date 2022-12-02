b9df386997c9b58b6fbf193b8d4195ce1b4e376f
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-12-01 21:24:48 -0500

    Time: 12 ms (97.78%), Space: 42.9 MB (95.56%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
@@ -1,6 +1,8 @@
 class Solution {
     public boolean closeStrings(String word1, String word2) {
-        if(word1.length()!=word2.length()) return false;
+        if(word1.length()!=word2.length()) {
+            return false;
+        }
         
         int[] w1=new int[26];
         int[] w2=new int[26];
