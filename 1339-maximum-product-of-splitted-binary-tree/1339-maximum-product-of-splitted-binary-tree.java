11bbe7dff4557d91821340a2ea109966d0502582
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2022-12-10 03:43:51 -0500

    Time: 16 ms (47.27%), Space: 57 MB (95.56%) - LeetHub

---
+++ 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    private List<Integer> allSums = new ArrayList<>();
+
+    public int maxProduct(TreeNode root) {
+        // long is a 64-bit integer.
+        long totalSum = treeSum(root);
+        long best = 0;
+        for (long sum : allSums) {
+            best = Math.max(best, sum * (totalSum - sum));
+        }
+        // We have to cast back to an int to match return value.
+        return (int)(best % 1000000007);
+
+    }
+
+    private int treeSum(TreeNode subroot) {
+        if (subroot == null) return 0;
+        int leftSum = treeSum(subroot.left);
+        int rightSum = treeSum(subroot.right);
+        int totalSum = leftSum + rightSum + subroot.val;
+        allSums.add(totalSum);
+        return totalSum;
+    }
+}
