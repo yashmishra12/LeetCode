4ff29c190a8119169846c3914ac3c622b3a30bce
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2025-02-27 22:40:25 -0500

    Update 283-move-zeroes.java

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -14,3 +14,33 @@ class Solution {
         }
     }
 }
+
+/*
+------------ ALTERNATIVE SOLUTION -------------
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int numberOfZeros = 0;
+        int nonZeroNumber = 0;
+        int currentPointer = 0;
+
+        while (currentPointer < nums.length) {
+            while (currentPointer < nums.length && nums[currentPointer] == 0) {
+                currentPointer++;
+            }
+
+            if (currentPointer >= nums.length) {
+                break;
+            }
+            nums[nonZeroNumber] = nums[currentPointer];
+            nonZeroNumber++;
+            currentPointer++;
+        }
+
+        while (nonZeroNumber < nums.length) {
+            nums[nonZeroNumber] = 0;
+            nonZeroNumber++;
+        }
+
+    }
+}
+*/
