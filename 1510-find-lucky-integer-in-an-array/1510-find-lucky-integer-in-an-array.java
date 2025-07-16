24da18830001b8eb37e47f3313105370c0d3d5c0
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2025-07-15 23:29:47 -0600

    Time: 5 ms (61.31%), Space: 43.4 MB (47.05%) - LeetHub

--- 1510-find-lucky-integer-in-an-array/1510-find-lucky-integer-in-an-array.java
+++ 1510-find-lucky-integer-in-an-array/1510-find-lucky-integer-in-an-array.java
@@ -1,20 +1,22 @@
 class Solution {
     public int findLucky(int[] arr) {
-        Map<Integer, Integer> m = new TreeMap<>(Collections.reverseOrder());
+        Map<Integer, Integer> m = new HashMap<>();
 
         for (int a : arr) {
             m.put(a, m.getOrDefault(a, 0) + 1);
         }
 
+        int answer = -1;
+
         for (Map.Entry<Integer, Integer> entry : m.entrySet()) {
             int key = entry.getKey();
             int val = entry.getValue();
 
-            if(key == val) {
-                return key;
+            if (key == val && key > answer) {
+                answer = key;
             }
         }
 
-        return -1;
+        return answer;
     }
 }
