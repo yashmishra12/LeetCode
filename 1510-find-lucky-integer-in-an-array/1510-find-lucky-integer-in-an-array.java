f537e349c451ba9a8ea98fe4e4880a04a46343a9
Yash Mishra <32962569+yashmishra12@users.noreply.github.com>
2025-07-15 23:32:40 -0600

    Time: 3 ms (66.09%), Space: 43.2 MB (85.95%) - LeetHub

--- 1510-find-lucky-integer-in-an-array/1510-find-lucky-integer-in-an-array.java
+++ 1510-find-lucky-integer-in-an-array/1510-find-lucky-integer-in-an-array.java
@@ -1,22 +1,40 @@
 class Solution {
     public int findLucky(int[] arr) {
-        Map<Integer, Integer> m = new HashMap<>();
+        // Map<Integer, Integer> m = new HashMap<>();
 
-        for (int a : arr) {
-            m.put(a, m.getOrDefault(a, 0) + 1);
-        }
+        // for (int a : arr) {
+        //     m.put(a, m.getOrDefault(a, 0) + 1);
+        // }
 
-        int answer = -1;
+        // int answer = -1;
 
-        for (Map.Entry<Integer, Integer> entry : m.entrySet()) {
-            int key = entry.getKey();
-            int val = entry.getValue();
+        // for (Map.Entry<Integer, Integer> entry : m.entrySet()) {
+        //     int key = entry.getKey();
+        //     int val = entry.getValue();
 
-            if (key == val && key > answer) {
-                answer = key;
+        //     if (key == val && key > answer) {
+        //         answer = key;
+        //     }
+        // }
+
+        // return answer;
+
+        Arrays.sort(arr);
+        int frq = 1;
+
+        for(int i=arr.length-1; i>=0; i--) {
+            frq = 1;
+            int curr = arr[i];
+            while(i!=0 && arr[i] == arr[i-1]) {
+                frq++;
+                i--;
+            }
+
+            if(frq == curr)  {
+                return curr;
             }
         }
 
-        return answer;
+        return -1;
     }
 }
